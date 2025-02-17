8ec71e0411a9e3e5625346e952bb79e4411559b5
Purwa <s.purwarajput@gmail.com>
2025-02-17 21:41:18 +0530

    search box

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,33 +1,43 @@
 import { Link } from 'react-router-dom';
 import Logo from '../../assets/images/logo.png';
 import CountryDropdown from '../CountryDropdown';
+import { IoSearch } from "react-icons/io5";
+import { Button } from 'react-bootstrap'; 
 
-const Header =()=>{
-    return(
+const Header = () => {
+    return (
         <>
-        <div className="headerWrapper">
-            <div className="top-strip bg-pink">
-                <div className="container">
-                    <p className="mb-0 mt-0 text-center"> Welcome to <b>SnapShop</b> - Your One-Stop Shop for Everything!</p>
-                </div>
-            </div>
-
-            <div className="header">
-                <div className="container">
-                    <div className="row">
-                        <div className="logoWrapper d-flex align-items-center col-sm-2">
-                            <Link to={'/'}><img src={Logo} alt='logo' /></Link>
-                        </div>
-
-                        <div className='col-sm-10 d-flex align-items-center part2'>
-                            <CountryDropdown/>
-                        </div>
+            <div className="headerWrapper">
+                <div className="top-strip bg-pink">
+                    <div className="container">
+                        <p className="mb-0 mt-0 text-center">
+                            Welcome to <b>SnapShop</b> - Your One-Stop Shop for Everything!
+                        </p>
                     </div>
                 </div>
+
+                <header className="header">
+                    <div className="container">
+                        <div className="row">
+                            <div className="logoWrapper d-flex align-items-center col-sm-2">
+                                <Link to={'/'}><img src={Logo} alt='logo' /></Link>
+                            </div>
+
+                            <div className='col-sm-10 d-flex align-items-center part2'>
+                                <CountryDropdown />
+
+                                {/* Header Search Start Here */}
+                                <div className='headerSearch d-flex align-items-center'>
+                                    <input type='text' placeholder='Search products...' />
+                                    <Button className='searchBtn'><IoSearch /></Button>
+                                </div>
+                                {/* Header Search ends here */}
+                            </div>
+                        </div>
+                    </div>
+                </header>
             </div>
-        </div>
         </>
     )
 }
-
 export default Header;
